71debe46a58e3d84dff238c09afd037a2cd2bafd
Mohamed Gamal <mohamadgamalbusiness@gmail.com>
2025-06-05 19:11:29 +0300

    __dirname

---
+++ test.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const imagesPath = path.join(__dirname, "images");
+console.log(imagesPath);
+
+
+console.log(__dirname) // C:\Users\medo\Desktop\learnNode
+console.log(__filename) // C:\Users\medo\Desktop\learnNode\test.js
